1db85913bf79adb90f515892fa6f1a361a863095
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2022-10-07 16:00:28 +0300

    bugFix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,46 +8,52 @@ import PizzaBlockSkeleton from '../components/PizzaBlock/PizzaBlockSkeleton';
 import '../scss/app.scss';
 
 function Home() {
-	const [pizzas, setPizzas] = useState([]);
-	const [activeCategory, setActiveCategory] = React.useState(0);
-	const [activeSort, setActiveSort] = useState({sort: 'rating', name:'популярностью', order: 'desc'});
-	const [isLoading, setIsLoading] = useState(false);
+  const [pizzas, setPizzas] = useState([]);
+  const [activeCategory, setActiveCategory] = React.useState(0);
+  const [activeSort, setActiveSort] = useState({
+    sort: 'rating',
+    name: 'популярностью',
+    order: 'desc',
+  });
+  const [isLoading, setIsLoading] = useState(false);
 
-	console.log(pizzas)
+  const fetchPizzas = async () => {
+    await axios
+      .get(
+        `https://633058daf5fda801f8df1ccd.mockapi.io/pizzas?${
+          activeCategory ? `category=${activeCategory}` : ''
+        }&sortBy=${activeSort.sort}&order=${activeSort.order}`,
+      )
+      .then((res) => setPizzas(res.data))
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(true));
+  };
 
-	const fetchPizzas = async () => {
-		await axios
-			.get(`https://633058daf5fda801f8df1ccd.mockapi.io/pizzas?${activeCategory ? `category=${activeCategory}` : ''}&sortBy=${activeSort.sort}&order=${activeSort.order}`)
-			.then((res) => setPizzas(res.data))
-			.catch((e) => console.log(e))
-			.finally(() => setIsLoading(true));
-	};
+  useEffect(() => {
+    setIsLoading(false);
+    fetchPizzas();
+  }, [activeCategory, activeSort]);
 
-	useEffect(() => {
-		setIsLoading(false)
-		fetchPizzas();
-	}, [activeCategory, activeSort]);
+  return (
+    <div className="content">
+      <div className="container">
+        <div className="content__top">
+          <Categories value={activeCategory} onChangeCategory={(item) => setActiveCategory(item)} />
+          <SortPopup value={activeSort} onChangeSortPopup={(item) => setActiveSort(item)} />
+        </div>
+        <h2 className="content__title">Усі піци</h2>
 
-	return (
-		<div className="content">
-			<div className="container">
-				<div className="content__top">
-					<Categories value={activeCategory} onChangeCategory={(item) => setActiveCategory(item)} />
-					<SortPopup value={activeSort} onChangeSortPopup={(item) => setActiveSort(item)} />
-				</div>
-				<h2 className="content__title">Усі піци</h2>
-
-				<div className="content__items">
-					{isLoading
-						? pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
-						: [...new Array(12)].map((_, index) => <PizzaBlockSkeleton key={index} />)}
-				</div>
-			</div>
-		</div>
-	);
+        <div className="content__items">
+          {isLoading
+            ? pizzas.map((obj) => <PizzaBlock key={obj.id} {...obj} />)
+            : [...new Array(12)].map((_, index) => <PizzaBlockSkeleton key={index} />)}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
 
-//сделать корзину
-
+//поиск
+//пагинацию
