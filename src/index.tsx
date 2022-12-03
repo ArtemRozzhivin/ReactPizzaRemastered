e04ce8dc7dc35cad83dfbd67d485871d1c99af92
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2022-12-03 17:16:55 +0200

    start TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <BrowserRouter>
     <App />
